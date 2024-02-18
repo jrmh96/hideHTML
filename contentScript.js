c93d103c234b3e24035338cba31fbbe2f3f9c68d
jrmh96 <jrmh96@gmail.com>
2024-02-17 20:40:36 -0500

    change contentScripts.js to spaces

--- contentScript.js
+++ contentScript.js
@@ -3,22 +3,22 @@
 
 // You will need to update "matches" at manifest.json for different sites
 let idBlockList = [
-	["hot-network-questions", "stackoverflow"],
-	["related", "youtube"]
+  ["hot-network-questions", "stackoverflow"],
+  ["related", "youtube"]
 ];
 
 // TODO: Consolidate this function with the ones in popup.js
 // e.g. setBlock(display: string)
 let checkBlock = (shouldBlock) => {
-	console.log("calling content script")
-	for (var idStrs of idBlockList) {
-		let originalBlock = document.getElementById(idStrs[0]);
-		if (originalBlock && shouldBlock) {
-			originalBlock.style.display = 'none';
-		}
-	}
+  console.log("calling content script")
+  for (var idStrs of idBlockList) {
+    let originalBlock = document.getElementById(idStrs[0]);
+    if (originalBlock && shouldBlock) {
+      originalBlock.style.display = 'none';
+    }
+  }
 }
 
 chrome.storage.sync.get("block", ({ block }) => {
-	checkBlock(block);
+  checkBlock(block);
 });
